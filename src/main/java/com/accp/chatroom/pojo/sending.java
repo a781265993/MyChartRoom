2e70d7e388df0c7c684e167ed2055f0b1b74da00
soul <781265993@qq.com>
2019-01-04 22:02:51 +0800

    2019-01-04晚：完整了聊天框的样式，将WebSorket.js中的取出来了

---
+++ src/main/java/com/accp/chatroom/pojo/sending.java
@@ -0,0 +1,36 @@
+package com.accp.chatroom.pojo;
+
+/**
+ * 
+* @ClassName: sending 
+* @Description: 消息基础信息描述类
+* @author 筠颜
+* @date 2019年1月4日 上午11:51:35 
+*
+ */
+public class sending {
+	
+	private String type;//发送消息类型
+	private String content;//发送消息内容
+	private Integer uid;//收件人id
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Integer getUid() {
+		return uid;
+	}
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+	
+	
+}
--- src/main/java/com/accp/chatroom/ws/MySocketHandler.java
+++ src/main/java/com/accp/chatroom/ws/MySocketHandler.java
@@ -18,9 +18,12 @@ import org.springframework.stereotype.Controller;
 
 import com.accp.chatroom.biz.WebMessageBiz;
 import com.accp.chatroom.pojo.friend;
+import com.accp.chatroom.pojo.sending;
 import com.accp.chatroom.pojo.user;
 import com.accp.chatroom.util.JsonObjectUtil;
 import com.accp.chatroom.ws.cfg.HttpSessionConfigurator;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 @ServerEndpoint(value = "/ws/sys", configurator = HttpSessionConfigurator.class)
 @Controller
@@ -30,9 +33,9 @@ public class MySocketHandler {
 	 */
 	private final static Map<Integer, Session> usersMap = new ConcurrentHashMap<Integer, Session>();// 线程安全
 	private Integer uId = null;// 用户的编号
-	
+
 	public static ApplicationContext ac;// 非常重要
-	
+
 	// 开启连接
 
 	@OnOpen
@@ -40,31 +43,29 @@ public class MySocketHandler {
 		WebMessageBiz messageBiz = (WebMessageBiz) ac.getBean("messageBiz");
 		HttpSession httpSession = (HttpSession) config.getUserProperties().get(HttpSession.class.getName());
 		final user _user = ((user) httpSession.getAttribute("user"));
-		if(_user != null) {
+		if (_user != null) {
 			this.uId = _user.getId();
 			usersMap.put(this.uId, session);// 存入会话信息
-			// 刷新好友列表:每30m一次
+			// 刷新好友列表:每15m一次
 			new Thread() {
 				public void run() {
-					//总数量
-					int Count = messageBiz.queryFriendList(_user).size();
-					//在线数量
+					// 在线数量
 					int Online = 0;
-					//离线数量
+					// 离线数量
 					int Offline = 0;
-					//申请数量
+					// 申请数量
 					int ApplyFor = 0;
 					for (friend f : messageBiz.queryFriendList(_user)) {
-						if(f.getType() == 0) {
+						if (f.getType() == 0) {
 							ApplyFor++;
-						} else if(f.getAide().getType() == 1) {
+						} else if (f.getAide().getType() == 1) {
 							Online++;
-						} else if(f.getAide().getType() == 0) {
+						} else if (f.getAide().getType() == 0) {
 							Offline++;
 						}
 					}
 					JsonObjectUtil util = new JsonObjectUtil();
-					util.setType("user");
+					util.setType("users");
 					try {
 						util.setContent(messageBiz.queryFriendList(_user));
 						usersMap.get(uId).getBasicRemote().sendText(util.toJSONString());
@@ -73,24 +74,40 @@ public class MySocketHandler {
 					}
 					while (true) {
 						try {
-							int count = messageBiz.queryFriendList(_user).size();
-							if(count > Count) {
+							// 在线数量
+							int online = 0;
+							// 离线数量
+							int offline = 0;
+							// 申请数量
+							int applyFor = 0;
+							for (friend f : messageBiz.queryFriendList(_user)) {
+								if (f.getType() == 0) {
+									online++;
+								} else if (f.getAide().getType() == 1) {
+									offline++;
+								} else if (f.getAide().getType() == 0) {
+									applyFor++;
+								}
+							}
+							if (online > Online || offline > Offline || applyFor > ApplyFor) {
 								util.removeContent();
 								util.setContent(messageBiz.queryFriendList(_user));
 								usersMap.get(uId).getBasicRemote().sendText(util.toJSONString());
-								Count = count;
+								Online = online;
+								Offline = offline;
+								ApplyFor = applyFor;
 							}
-							Thread.sleep(30*1000);
+							Thread.sleep(15 * 1000);
 						} catch (Exception e) {
 							break;
 						}
 					}
 				}
 			}.start();
-			//刷新聊天消息
-			
+			// 刷新聊天消息
+
 		} else {
-			this.onerror(session,new Throwable());
+			this.onerror(session, new Throwable());
 		}
 	}
 
@@ -99,7 +116,7 @@ public class MySocketHandler {
 	public void onclose(Session session) {
 		WebMessageBiz messageBiz = (WebMessageBiz) ac.getBean("messageBiz");
 		System.out.println("长连接关闭");
-		if(uId != null) {
+		if (uId != null) {
 			usersMap.remove(uId);
 			user User = new user();
 			User.setId(uId);
@@ -122,12 +139,15 @@ public class MySocketHandler {
 	// 发送消息 这个方法参数的不能更改
 	@OnMessage
 	public void onmessage(String msg, Session session) {
-		System.out.println("客户端【" + session.getId() + "】通讯，消息：" + msg);
 		// 发消息 pong
 		try {
 			if (!"ping".equals(msg)) {
-				System.out.println(msg);
-//				sendUsers(msg);
+				sending obj = JSON.parseObject(msg, sending.class);
+				if ("sendUser".equals(obj.getType())) {
+					sendUser(obj.getUid(), obj.getContent());
+				} else if ("sendUsers".equals(obj.getType())) {
+					sendUsers(obj.getContent());
+				}
 			}
 		} catch (Exception e) {
 			System.out.println("发送异常");
@@ -136,7 +156,7 @@ public class MySocketHandler {
 	}
 
 	// 单发
-	private void sendUsers(Integer uid,String msg) {
+	private void sendUser(Integer uid, String msg) {
 		try {
 			usersMap.get(uid).getBasicRemote().sendText(msg);
 		} catch (IOException e) {
@@ -144,4 +164,14 @@ public class MySocketHandler {
 		}
 	}
 
+	// 群发
+	private void sendUsers(String msg) {
+		for (Session session : usersMap.values()) {
+			try {
+				session.getBasicRemote().sendText(msg);
+			} catch (IOException e) {
+				continue;
+			}
+		}
+	}
 }
